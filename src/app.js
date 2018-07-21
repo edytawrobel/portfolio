f10ffb0043e446c958bfb0cb9c8a158ca59038b8
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-07-21 11:10:13 +0100

    Data flow: upstream and downstream. State defined and changed in component itslef. Props always come from above. Error as a component state and rendering it to screen

--- src/app.js
+++ src/app.js
@@ -1,14 +1,58 @@
 class IndecisionApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            options: []
+        }
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            }
+        });
+    }
+
+    handlePick() {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const randomOption = this.state.options[randomNum];
+        alert(randomOption);
+    }
+
+    handleAddOption(option) {
+        if (!option) {
+            return 'Enter valid value to add item';
+            // that means we already found a match for this option, it exists and we don't want duplicates
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists!';
+        }
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat(option)
+            }
+        });
+    }
+
     render() {
         const title = 'Indecision app';
         const subtitle = 'Put your life in the hands of a computer';
-        const options = [' thing one ', 'thing two ', 'thing three '];
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
-                <Action />
-                <Options options={options}/>
-                <AddOption />
+                <Action 
+                    hasOptions={this.state.options.length > 0}
+                    handlePick={this.handlePick}
+                
+                />
+                <Options 
+                    options={this.state.options} 
+                    handleDeleteOptions={this.handleDeleteOptions}/>
+                <AddOption 
+                    handleAddOption={this.handleAddOption}/>
             </div>
         )
     }
@@ -26,30 +70,19 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-    handlePick() {
-        alert('woohaa!');
-    }
-
     render() {
         return (
             <div>
-                <button onClick={this.handlePick}>What should I do?</button>
+                <button 
+                    onClick={this.props.handlePick}
+                    disabled={!this.props.hasOptions}
+                >What should I do?</button>
             </div>
         );
     } 
 }
 
 class Options extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    }
-
-    handleRemoveAll() {
-        console.log(this.props.options);
-        alert('removed');
-    }
-
     render() {
         return (
             <div>
@@ -61,7 +94,7 @@ class Options extends React.Component {
             )
         }
         </ul>
-        <button onClick={this.handleRemoveAll}>Remove All</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
             </div>
         );
     }
@@ -76,20 +109,30 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
+    
     handleAddOption(e) {
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(option);
 
-        if(option) {
-            alert(option);
-        } else {
-            alert('no option provided!');
-        }
+        this.setState(() => {
+            return {
+                error: error
+            }
+        });
     }
 
     render() {
         return (
             <div>
+                {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.handleAddOption}>
                     <input type="text" name="option" placeholder="your option"></input>
                     <button>Add Option</button>
