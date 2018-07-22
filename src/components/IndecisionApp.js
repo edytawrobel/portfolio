d14828d1193fb910d634941b2552f4ebac5da8b0
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-07-22 21:27:45 +0100

    Pass in jsx with props.children

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,6 +3,7 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import Layout from './Layout';
 
 class IndecisionApp extends React.Component {
 
@@ -58,11 +59,11 @@ class IndecisionApp extends React.Component {
         const subtitle = 'Put your life in the hands of a computer';
         return (
             <div>
+                <Layout />
                 <Header title={title} subtitle={subtitle} />
                 <Action 
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick}
-                
                 />
                 <Options 
                     options={this.state.options} 
