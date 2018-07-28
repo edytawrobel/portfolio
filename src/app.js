9c0d35dcf34b0a98ab06e7b6a53a3f6b65e1ab79
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-07-28 11:59:02 +0100

    Add NavLink for extra link props

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
@@ -38,10 +38,10 @@ const NotFound = () => (
 const Header = () => (
     <header>
         <h1>Expensify</h1>
-        <Link to="/">Home </Link>
-        <Link to="/create">Create </Link>
-        <Link to="/edit">Edit </Link>
-        <Link to="/help">Help </Link>
+        <NavLink to="/" activeClassName="is-active" exact="true">Home </NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create </NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit </NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help </NavLink>
     </header>
 )
 
